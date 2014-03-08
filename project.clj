2a04d2213c199b90a37e3cee0fc31e9e9b65507e
Oliver Godby <oliver_godby@me.com>
2014-03-08 01:40:55 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject toggler "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [liberator "0.11.0"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler toggler.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
