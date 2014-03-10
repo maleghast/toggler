aeb93f9528ed12ee29722b5dd85406ece9f187ae
Oliver Godby <oliver_godby@me.com>
2014-03-10 01:56:10 +0000

    Toggler First Commit

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [liberator "0.11.0"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [cheshire "5.3.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler toggler.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src/toggler/handler.clj
+++ src/toggler/handler.clj
@@ -1,12 +1,46 @@
 (ns toggler.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [liberator.core :refer [resource defresource]]
+            [cheshire.core :refer :all]))
+
+(def cfg (atom (decode (slurp "resources/config.json") true)))
+
+(defn getoggleall [component]
+  (get @cfg (keyword component)))
+
+(defn getoggle [component setting]
+  (get-in @cfg [(keyword component) (keyword setting)]))
+
+(defresource status
+  :allowed-methods [:get]
+  :available-media-types ["application/json"]
+  :handle-ok "Toggler - This is not a functional endpoint; Sorry! :-(")
+
+(defresource get-toggle [component setting]
+  :allowed-methods [:get]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (getoggle component setting)))
+
+(defresource get-toggle-all [component]
+  :allowed-methods [:get]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (getoggleall component)))
+
+(comment
+  (defresource set-toggle [component setting]
+  :allowed-mothods [:put]
+  :available-media-types ["application/json"]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
-  (route/not-found "Not Found"))
+  (GET "/" [] status)
+  (GET "/toggle/:component" [component] (get-toggle-all component))
+  (GET "/toggle/:component/:setting" [component setting] (get-toggle component setting))
+  (comment (PUT "/toggle/:component/:setting" [component setting value] (set-toggle component setting value)))
+  (comment (POST "/toggle/new" {params :form-params} (create-toggle params)))
+  (route/not-found "Not Found")
+  (route/resources "/"))
 
 (def app
   (handler/site app-routes))
