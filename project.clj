2bf7faff53a7c1088cceb8b91f146b8d9b3b6bc8
Oliver Godby <oliver_godby@me.com>
2014-09-12 14:51:16 +0100

    latest deps that work

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject toggler "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [liberator "0.11.0"]
-                 [compojure "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.12.1"]
+                 [compojure "1.1.8"]
                  [cheshire "5.3.1"]
-                 [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
+                 [ring "1.3.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler toggler.handler/app}
   :profiles
