c7a11db3fed58ce5b455511f7a461fe0b2348d85
Oliver Godby <oliver_godby@me.com>
2015-02-05 22:30:53 +0800

    updating library / dep version

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [liberator "0.12.1"]
-                 [compojure "1.1.8"]
+                 [liberator "0.12.2"]
+                 [compojure "1.3.1"]
                  [cheshire "5.3.1"]
-                 [ring "1.3.1"]]
-  :plugins [[lein-ring "0.8.10"]]
+                 [ring "1.3.2"]]
+  :plugins [[lein-ring "0.9.1"]]
   :ring {:handler toggler.handler/app}
   :resource-paths ["src/toggler/resources"]
   :main toggler.handler
