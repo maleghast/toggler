cadf3c542a11d5ad0d7cc964ef625ebb0e940f53
C. Oliver Godby <oliver@zipmatch.com>
2016-04-15 08:59:32 +0800

    Updated Clojure to 1.8 and updated dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject toggler "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [liberator "0.12.2"]
-                 [compojure "1.3.1"]
-                 [cheshire "5.3.1"]
-                 [ring "1.3.2"]]
-  :plugins [[lein-ring "0.9.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [liberator "0.14.1"]
+                 [compojure "1.5.0"]
+                 [cheshire "5.6.1"]
+                 [ring "1.4.0"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler toggler.handler/app}
   :resource-paths ["src/toggler/resources"]
   :main toggler.handler
