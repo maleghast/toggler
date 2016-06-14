3c4cd695ad903e245074a0df1c78678b089b34d0
Oliver Godby <oliver_godby@me.com>
2016-06-14 16:08:54 +0800

    updating version of ring

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [liberator "0.14.1"]
                  [compojure "1.5.0"]
                  [cheshire "5.6.1"]
-                 [ring "1.4.0"]]
+                 [ring "1.5.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler toggler.handler/app}
   :resource-paths ["src/toggler/resources"]
